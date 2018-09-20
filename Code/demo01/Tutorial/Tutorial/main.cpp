baebfb11ca4c92368e919c77fd71821f168cf962
chenlonglong <chenlonglong@qiyi.com>
2018-09-20 22:45:12 +0800

    aad code

--- Code/demo01/Tutorial/Tutorial/main.cpp
+++ Code/demo01/Tutorial/Tutorial/main.cpp
@@ -1,6 +1,23 @@
 #include <Windows.h>
 #include <cstdio>
 
+#include "libavcodec\avcodec.h"
+#include "libavformat\avformat.h"
+#include "libswscale\swscale.h"
+
+void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame) 
+{
+
+    FILE *pFile = nullptr;
+    char szFilename[32] = { 0 };
+
+    int y;
+
+    sprintf(szFilename, "fram");
+
+}
+
+
 int main() 
 {
     system("pause");
