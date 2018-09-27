be0b9acb400850f04d27bf76afbe8417b845d2fd
chenlonglong <chenlonglong.qiyi.com>
2018-09-27 10:27:28 +0800

    add source

---
+++ Code/demo01/Mac/Demo01/Demo01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Demo01
+//
+//  Created by multimedia on 2018/9/26.
+//  Copyright © 2018年 ___multiMedia___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
